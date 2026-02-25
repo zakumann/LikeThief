0f430d6d47ad742fb69e3c6866b3101a020b1e3f
zakumann <samuel742able@gmail.com>
2026-02-26 01:41:01 +0900

    Add AI Sight Perception

--- Source/LikeThief/Private/Character/Enemy/EnemyAI.cpp
+++ Source/LikeThief/Private/Character/Enemy/EnemyAI.cpp
@@ -2,6 +2,31 @@
 
 
 #include "Character/Enemy/EnemyAI.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Perception/AISense_Sight.h"
+#include "Perception/AISense_Hearing.h"
+
+AEnemyAI::AEnemyAI()
+{
+	// Create AI Perception Component
+	AIPerception = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerception"));
+
+	// Create Sight Config
+	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("SightConfig"));
+	SightConfig->SightRadius = 1000.0f;
+	SightConfig->LoseSightRadius = 1800.0f;
+	SightConfig->PeripheralVisionAngleDegrees = 75.0f;
+	SightConfig->DetectionByAffiliation.bDetectEnemies = true;
+	SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
+	SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
+
+	// Configure AI Perception
+	AIPerception->ConfigureSense(*SightConfig);
+	AIPerception->SetDominantSense(SightConfig->GetSenseImplementation());
+
+	// Bind Perception Update Event
+	AIPerception->OnTargetPerceptionUpdated.AddDynamic(this, &AEnemyAI::OnTargetPerceptionUpdated);
+}
 
 void AEnemyAI::BeginPlay()
 {
@@ -10,5 +35,58 @@ void AEnemyAI::BeginPlay()
 	if (BehaviorTree)
 	{
 		RunBehaviorTree(BehaviorTree);
+	}
+}
+
+void AEnemyAI::OnTargetPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
+	// Get Scene Class for Stimulus
+	TSubclassOf<UAISense> SenseClass = UAIPerceptionSystem::GetSenseClassForStimulus(GetWorld(), Stimulus);
+
+	// Handle Sense
+	HandleSense(Actor, Stimulus);
+}
+
+void AEnemyAI::HandleSense(AActor* SensedActor, const FAIStimulus& Stimulus)
+{
+	if (!SensedActor)
+	{
+		return;
+	}
+
+	UBlackboardComponent* BlackboardComp = GetBlackboardComponent();
+	if (!BlackboardComp)
+	{
+		return;
+	}
+
+	// Get Sense Class
+	TSubclassOf<UAISense> SenseClass = UAIPerceptionSystem::GetSenseClassForStimulus(GetWorld(), Stimulus);
+
+	// Switch on Sense Class
+	if (SenseClass == UAISense_Sight::StaticClass())
+	{
+		// AISense_Sight
+
+		// Set Value as Bool - IsInvestigating
+		bool bSuccessfullySensed = Stimulus.WasSuccessfullySensed();
+		BlackboardComp->SetValueAsBool(FName("IsInvestigating"), bSuccessfullySensed);
+
+		// Branch
+		if (bSuccessfullySensed)
+		{
+			// Set Value as Object - TargetLocationActor
+			BlackboardComp->SetValueAsObject(FName("TargetLocationActor"), SensedActor);
+		}
+		else
+		{
+			// Lost sight - clear target
+			BlackboardComp->ClearValue(FName("TargetLocationActor"));
+		}
+	}
+	else if (SenseClass == UAISense_Hearing::StaticClass())
+	{
+		// AISense_Hearing (later)
+		// Current empty
 	}
 }
--- Source/LikeThief/Public/Character/Enemy/EnemyAI.h
+++ Source/LikeThief/Public/Character/Enemy/EnemyAI.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "Perception/AISenseConfig_Hearing.h"
 #include "EnemyAI.generated.h"
 
 /**
@@ -14,9 +17,27 @@ class LIKETHIEF_API AEnemyAI : public AAIController
 {
 	GENERATED_BODY()
 
+public:
+	AEnemyAI();
+
 protected:
 	virtual void BeginPlay() override;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
 	UBehaviorTree* BehaviorTree;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
+	UAIPerceptionComponent* AIPerception;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+	UAISenseConfig_Sight* SightConfig;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+	UAISenseConfig_Hearing* HearingConfig;
+
+private:
+	UFUNCTION()
+	void OnTargetPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+
+	void HandleSense(AActor* SensedActor, const FAIStimulus& Stimulus);
 };
